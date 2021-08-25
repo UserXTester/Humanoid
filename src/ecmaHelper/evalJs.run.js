87d6f0822351a97ea93ab98b2cc14ec392f88997
Phoenix Error <88020281+PhoenixError@users.noreply.github.com>
2021-08-25 20:47:07 +0530

    Create evalJs.run.js

---
+++ src/ecmaHelper/evalJs.run.js
@@ -0,0 +1,9 @@
+// Humanoid - UserBot
+// Copyright (C) 2020 TeamHumanoid
+//
+// This file is a part of < https://github.com/TeamHumanoid/Humanoid/ >
+// PLease read the GNU Affero General Public License in
+// <https://www.github.com/TeamHumanoid/Humanoid/blob/main/LICENSE/>.
+
+'use-strict';
+sgrywryhio
