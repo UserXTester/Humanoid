8ea75453b02e3961eb2593240a21f11c788f303d
Phoenix Error <88020281+PhoenixError@users.noreply.github.com>
2021-08-26 09:54:07 +0530

    Humanoid-Userbot

---
+++ assistant/ping.py
@@ -0,0 +1,20 @@
+# Humanoid - UserBot
+# Copyright (C) 2021 TeamHumanoid
+#
+# This file is a part of < https://github.com/TeamHumanoid/Humanoid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamHumanoid/Humanoid/blob/main/LICENSE/>.
+
+from datetime import datetime
+
+
+@asst_cmd("ping$")
+@owner
+async def _(event):
+    start = datetime.now()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await asst.send_message(
+        event.chat_id,
+        f"**Pong!!**\n `{ms} milliseconds`",
+    )
