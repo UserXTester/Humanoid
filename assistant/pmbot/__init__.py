34d9751ac9379cc21c7c1eb4ff7b0546235a4d9c
Phoenix Error <88020281+PhoenixError@users.noreply.github.com>
2021-08-26 09:44:32 +0530

    Create __init__.py

---
+++ assistant/pmbot/__init__.py
@@ -0,0 +1,7 @@
+from pyHumanoid.functions.asst_fns import *
+from pyHumanoid.functions.botchat_db import *
+
+from .. import *
+
+OWNER_NAME = Humanoid_bot.me.first_name
+OWNER_ID = Humanoid_bot.me.id
