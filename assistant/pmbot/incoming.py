eba789abbef2672505d5c885a7a5a1172c977082
Phoenix Error <88020281+PhoenixError@users.noreply.github.com>
2021-08-26 09:46:15 +0530

    Add files via upload

---
+++ assistant/pmbot/incoming.py
@@ -0,0 +1,33 @@
+# Humanoid - UserBot
+# Copyright (C) 2021 TeamHumanoid
+#
+# This file is a part of < https://github.com/TeamHumanoid/Humanoid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamHumanoid/Humanoid/blob/main/LICENSE/>.
+
+# https://github.com/xditya/TeleBot/blob/master/telebot/plugins/mybot/pmbot/incoming.py
+"""
+Incoming Message(s) forwarder.
+"""
+
+from telethon import events
+
+from . import *
+
+# if incoming
+
+
+@asst.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+async def on_new_mssg(event):
+    incoming = event.raw_text
+    who = event.sender_id
+    if is_blacklisted(who):
+        return
+    # doesn't reply to that user anymore
+    if incoming.startswith("/"):
+        pass
+    elif who == OWNER_ID:
+        return
+    else:
+        xx = await event.forward_to(OWNER_ID)
+        add_stuff(xx.id, who)
