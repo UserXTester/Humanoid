65bd6a55116ed2dcc2f51fe6cfd68db05caaac08
Phoenix Error <88020281+PhoenixError@users.noreply.github.com>
2021-08-26 09:43:04 +0530

    Create __init__.py

---
+++ assistant/manager/__init__.py
@@ -0,0 +1,11 @@
+# Humanoid - UserBot
+# Copyright (C) 2021 TeamHumanoid
+#
+# This file is a part of < https://github.com/TeamHumanoid/Humanoid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamHumanoid/Humanoid/blob/main/LICENSE/>.
+
+
+from plugins import *
+
+asst = asst
