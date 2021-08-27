a90f9c4a62f4f74802122552a82263cdf46db0f7
Phoenix Error <88020281+PhoenixError@users.noreply.github.com>
2021-08-27 07:43:33 +0530

    Update volume.py

--- vcbot/volume.py
+++ vcbot/volume.py
@@ -1,6 +1,5 @@
 # Humanoid - UserBot
 # Copyright (C) 2021 TeamHumanoid
-#
 # This file is a part of < https://github.com/TeamHumanoid/Humanoid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamHumanoid/Humanoid/blob/main/LICENSE/>.
